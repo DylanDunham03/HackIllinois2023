8e27aaec36839df2d6063b02554aa627684eb13f
Dylan <dylanad2@illinois.edu>
2023-02-25 17:55:56 -0600

    fixed button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 function SearchBar(props) {
   const [searchInput, setSearchInput] = useState("");
 
+//  console.log(searchInput);
 
  const [data,setData]=useState(null)
  const [print,setPrint]=useState(false)
@@ -32,16 +33,14 @@ function getData(val)
                 aria-describedby="button-addon1"
                 onChange={handleChange}
                 value={searchInput} />
-              <button onClick = {(e) => props.onClick(e,searchInput )}> 
-                  click
-              </button>
-              {/* <button
+              <button onClick = {(e) => props.onClick(e,searchInput )}
                 class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium border border-solid border-neutral-300 uppercase leading-tight text-white shadow-xl transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
                 type="button"
                 id="button-addon1"
                 data-te-ripple-init
-                data-te-ripple-color="light"
-                onClick = {props.onCLick}> 
+                data-te-ripple-color="light"              
+              
+              > 
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
@@ -52,7 +51,8 @@ function getData(val)
                     d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
                     clip-rule="evenodd" />
                 </svg>
-              </button> */}
+              </button>
+
             </div>
           </div>
         </div>
