c6a24be90e19b17aa823c9b5890d1492a91d6ccf
Dylan <dylanad2@illinois.edu>
2023-02-25 15:06:30 -0600

    good

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -69,7 +69,7 @@ return <div>
                 value={searchInput} />
                 
               <button
-                class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+                class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium border border-solid border-neutral-300 uppercase leading-tight text-white shadow-xl transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
                 type="button"
                 id="button-addon1"
                 data-te-ripple-init
