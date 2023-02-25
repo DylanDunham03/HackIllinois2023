cebf356ec6f92a967755908af64d9441325f1058
Dylan <dylanad2@illinois.edu>
2023-02-25 17:07:37 -0600

    api first works

--- src/App.js
+++ src/App.js
@@ -1,31 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar'
-
+import { useState } from 'react';
 // comment
 
 var current_input = ""
 
 // var queryURL = "https://api.geoapify.com/v1/geocode/search?text=" + city + "&appid=" + APIKey;
 
-var api_key = "8c18e6ad3df8403b9ff3203de82711f9"
-var test = "https://api.geoapify.com/v1/geocode/search?text=1214-1224%20West%20Van%20Buren%20Street%2C%20Chicago%2C%20IL%2060607%2C%20United%20States%20of%20America&lang=en&limit=5&format=json&apiKey=" + api_key;
 
 
+// const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-var requestOptions = {
-  method: 'GET',
-};
+// let obj = geoFunction();
+// sleep(2000)
+// while (obj == null) {
+//   await sleep(1000);
+// }
+// console.log("anythong?")
+// console.log(obj)
 
-var answer;
 
-fetch(test, requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// async function renderData() {
+//   let obj = geoFunction();
+
+//   let html = '';
+//   obj.forEach(user => {
+//       let htmlSegment = `<div class="user">
+//                           <img src="${user.profileURL}" >
+//                           <h2>${user.firstName} ${user.lastName}</h2>
+//                           <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+//                       </div>`;
+
+//       html += htmlSegment;
+//   });
+
+//   let container = document.querySelector('.container');
+//   container.innerHTML = html;
+// }
+
+// renderData()
+
+
 
 
 function App() {
+  const [locationData, updateLocationData] = useState({});
+  const baseUrl = "https://api.geoapify.com/v1/geocode/search?text="
+  const specifications = "&filter=countrycode:us&limit=5&format=json&apiKey=8c18e6ad3df8403b9ff3203de82711f9"
+//  var test = "https://api.geoapify.com/v1/geocode/search?text=1214-1224%20West%20Van%20Buren%20Street%2C%20Chicago%2C%20IL%2060607%2C%20United%20States%20of%20America&lang=en&limit=5&format=json&apiKey=" + api_key;
+
+  var test = baseUrl+ "Main%20St.%2C%20Berlin" + specifications;
+
+
+
+  var requestOptions = {
+    method: 'GET',
+    // body: lon
+  };
+
+  function makeAddress(e) {
+    let finalString = '';
+    for (let i = 0; i < e.length; i++) {
+      if (e[i] == ' ') {
+        finalString += "%20"
+      } else 
+        finalString += e[i]
+    }
+    return baseUrl + finalString + specifications;
+  }
+
+
+  async function geoFunction(e, input) {
+   
+    let updatedAddress = makeAddress(input)
+    console.log(updatedAddress)
+    await fetch(updatedAddress, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      updateLocationData(data.results[0])
+    })
+    .catch(error => console.log('error', error));
+
+
+  
+}
+
   return (
     <div className="App">
       <header className="App-header">
@@ -39,11 +100,12 @@ function App() {
         </h1> */}
 
         <div classname='App'>
-        <SearchBar/>
+        <SearchBar onClick ={geoFunction}/>
         </div> 
 
 
-
+        <h2>{locationData.lon}</h2>
+        <h2>{locationData.country}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,7 @@
 import React, {useState} from 'react'
+function SearchBar(props) {
+  const [searchInput, setSearchInput] = useState("");
 
-
-const SearchBar = () => {
-
- const [searchInput, setSearchInput] = useState("");
-
- console.log(searchInput);
 
  const [data,setData]=useState(null)
  const [print,setPrint]=useState(false)
@@ -22,15 +18,7 @@ function getData(val)
   setPrint(false)
 }
 
-return <div>
-
-{/* <input
-   type="search"
-   placeholder="Search here"
-   onChange={handleChange}
-   value={searchInput} /> */}
-
-   
+  return (
 
 <div class="flex justify-center">
           <div class="mb-3 xl:w-96">
@@ -44,13 +32,16 @@ return <div>
                 aria-describedby="button-addon1"
                 onChange={handleChange}
                 value={searchInput} />
-                
-              <button
+              <button onClick = {(e) => props.onClick(e,searchInput )}> 
+                  click
+              </button>
+              {/* <button
                 class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium border border-solid border-neutral-300 uppercase leading-tight text-white shadow-xl transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
                 type="button"
                 id="button-addon1"
                 data-te-ripple-init
-                data-te-ripple-color="light">
+                data-te-ripple-color="light"
+                onClick = {props.onCLick}> 
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
@@ -61,35 +52,104 @@ return <div>
                     d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
                     clip-rule="evenodd" />
                 </svg>
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
 
+  );
+}
+
+// const SearchBar = (props) => {
+
+//  const [searchInput, setSearchInput] = useState("");
+
+//  console.log(searchInput);
+
+//  const [data,setData]=useState(null)
+//  const [print,setPrint]=useState(false)
+
+// const handleChange = (e) => {
+//   e.preventDefault();
+//   setSearchInput(e.target.value);
+// };
+
+// function getData(val)
+// {
+//   // console.warn(val.target.value)
+//   setData(val.target.value)
+//   setPrint(false)
+// }
+
+// return <div>
+
+// {/* <input
+//    type="search"
+//    placeholder="Search here"
+//    onChange={handleChange}
+//    value={searchInput} /> */}
+
+   
+
+// <div class="flex justify-center">
+//           <div class="mb-3 xl:w-96">
+//             <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+//               <input
+//                 type="search"
+//                 // class = "text-white"
+//                 class="relative m-0 -mr-px block w-[1%] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-white outline-none transition duration-300 ease-in-out focus:border-primary focus:text-white focus:shadow-te-primary focus:outline-none dark:text-white dark:placeholder:text-white"
+//                 placeholder="Search"
+//                 aria-label="Search"
+//                 aria-describedby="button-addon1"
+//                 onChange={handleChange}
+//                 value={searchInput} />
+                
+//               <button
+//                 class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium border border-solid border-neutral-300 uppercase leading-tight text-white shadow-xl transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+//                 type="button"
+//                 id="button-addon1"
+//                 data-te-ripple-init
+//                 data-te-ripple-color="light"
+//                 onClick = {props.onCLick}> 
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   viewBox="0 0 20 20"
+//                   fill="currentColor"
+//                   class="h-5 w-5">
+//                   <path
+//                     fill-rule="evenodd"
+//                     d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+//                     clip-rule="evenodd" />
+//                 </svg>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
 
 
-{/* 
-<table>
-  <tr>
-    <th>Country</th>
-    <th>Continent</th>
-  </tr>
 
-{countries.map((country, *index*) => {
+// {/* 
+// <table>
+//   <tr>
+//     <th>Country</th>
+//     <th>Continent</th>
+//   </tr>
 
-<div>
-  <tr>
-    <td>{country.name}</td>
-    <td>{country.continent}</td>
-  </tr>
-</div>
+// {countries.map((country, *index*) => {
 
-})}
-</table> */}
+// <div>
+//   <tr>
+//     <td>{country.name}</td>
+//     <td>{country.continent}</td>
+//   </tr>
+// </div>
 
-</div>
+// })}
+// </table> */}
+
+// </div>
 
 
-};
+// };
 
 export default SearchBar;
