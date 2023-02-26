6b1813060a86dd726db72086cd241d53dceb6724
Dylan <dylanad2@illinois.edu>
2023-02-25 21:26:28 -0600

    added comp

--- src/pages/informationPage.js
+++ src/pages/informationPage.js
@@ -17,22 +17,59 @@ import {
   import AnimatedProgressProvider from "./AnimatedProgressProvider";
   import ChangingProgressProvider from "./ChangingProgressProvider";
   import RadialSeparators from "./RadialSeparators";
+
+import ReactDOM from "react-dom";
+import ProgressLine from "./ProgressLine";
   
 
 
   const risk_num = 66;
 
+  const fire_num = .32;
+
+  export var ProgressBar = ({ width, percent }) => {
+
+    const [value, setValue] = React.useState(0);
+  
+    React.useEffect(() => {
+      setValue(percent * width);
+    });
+  
+    return (
+      <div>
+        <div className="progress-div" style={{ width: width }}>
+          <div style={{ width: `${value}px` }} className="progress" />
+        </div>
+      </div>
+    );
+  };
+
+  
 
 
 function InformationPage(props) {
-
+    
+    
     const navigate = useNavigate();
     async function returnHome() {
         navigate('/')
         }
-
   return (
-      <header className="App-header">
+    <header className="App-header">
+    <div class="pb-10 left-0 top-0 " >
+    <h2
+          style={{
+            position: "absolute",
+            left: `${0}px`,
+            top: `${45}px`,
+          }}
+        >
+            </h2>
+        <button  onClick={returnHome}
+        class="bg-[#E9E9E9] hover:bg-[#B9B9B9] text-[#282c34] font-bold py-2 px-4 rounded-full">
+        Home
+        </button>
+    </div>
 <div>
 
 
@@ -44,13 +81,29 @@ function InformationPage(props) {
     
 </div>
 
-<div>
-<button  onClick={returnHome}
-class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-  Home
-</button>
+<div class="relative mb-4 flex flex-col items-stretch pt-4">
+<div class="pt-4">
+<ProgressBar width={400} percent={fire_num} />
+</div>
 
-    </div>
+<div class="pt-4">
+<ProgressBar width={400} percent={fire_num} />
+</div>
+
+<div class="pt-4">
+<ProgressBar width={400} percent={fire_num} />
+</div>
+
+<div class="pt-4">
+<ProgressBar width={400} percent={fire_num} />
+</div>
+
+<div class="pt-4">
+<ProgressBar width={400} percent={fire_num} />
+</div>
+</div>
+
+
       </header>
 
     
